01732dd2f598da0066d5e6a159f9a8c25d33334a
Nilana Rocha <nilanaarquitetura@gmail.com>
2020-08-05 17:53:39 +1000

    creating a new file called assets that contain the logo for the website

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Menu from './components/Menu';
 
 function App() {
-  return <di></di>;
+  return (
+    <div>
+      <Menu />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Menu/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png';
+
+function Menu() {
+  return (
+    <nav>
+      <img src={Logo} alt="NikaFlix logo" />
+    </nav>
+  );
+}
+
+export default Menu;
